e4a59c69baeb14b319b54c3c4b2669a25f77d380
James Lee <jameslee777@yahoo.com>
2014-07-05 15:59:06 -0500

    test

--- src/daemon/daemon.cpp
+++ src/daemon/daemon.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 // node.cpp : Defines the entry point for the console application.
-//
+// test
 
 
 #include "include_base_utils.h"
