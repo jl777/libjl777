0c8f0a3c8edd2beeae5175054ab180483aed57b1
jl777 <jl777@users.noreply.github.com>
2014-07-06 16:16:48 -0500

    Update uv.h

--- NXTservices/libuv/include/uv.h
+++ NXTservices/libuv/include/uv.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /* Copyright Joyent, Inc. and other Node contributors. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -2341,6 +2341,4 @@ struct uv_loop_s {
 }
 #endif
 #endif /* UV_H */
-=======
-fff
->>>>>>> FETCH_HEAD
+
