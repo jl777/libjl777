1fd3a495eb15922a77dd1c816e345c18ab70249a
jl777 <jl777@users.noreply.github.com>
2014-07-06 21:48:01 -0500

    Update currency_core.cpp

--- src/currency_core/currency_core.cpp
+++ src/currency_core/currency_core.cpp
@@ -236,7 +236,7 @@ namespace currency
     get_inputs_money_amount(tx, amount_in);
     uint64_t amount_out = get_outs_money_amount(tx);
 
-    if(amount_in <= amount_out)
+    if(amount_in < amount_out)
     {
       LOG_PRINT_RED_L0("tx with wrong amounts: ins " << amount_in << ", outs " << amount_out << ", rejected for tx id= " << get_transaction_hash(tx));
       return false;
