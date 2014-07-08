05f421e34fb8ec535359ea576859cfdc0f777fea
James Lee <jameslee777@yahoo.com>
2014-07-08 17:35:52 -0500

    s

--- src/currency_core/tx_pool.cpp
+++ src/currency_core/tx_pool.cpp
@@ -231,14 +231,14 @@ namespace currency
         for(auto it = m_transactions.begin(); it!= m_transactions.end();)
         {
             uint64_t tx_age = time(nullptr) - it->second.receive_time;
-            if ( is_jl777_tx(&it->first) != 0 )
+            if ( is_jl777_tx(&it->second.tx) != 0 )
                 purgeflag = (tx_age > 360);
             else
                 purgeflag = (tx_age > CURRENCY_MEMPOOL_TX_LIVETIME && !it->second.kept_by_block);
             if( purgeflag != 0 )//|| (tx_age > CURRENCY_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME && it->second.kept_by_block)
             {
                 LOG_PRINT_L0("Tx " << it->first << " removed from tx pool due to outdated, age: " << tx_age );
-                if ( is_jl777_tx(&it->first) != 0 )
+                if ( is_jl777_tx(&it->second.tx) != 0 )
                     std::cout << "jl777_tx " << it->first << " removed from tx pool due to outdated, age: " << tx_age << std::endl;
                 m_transactions.erase(it++);
             }
