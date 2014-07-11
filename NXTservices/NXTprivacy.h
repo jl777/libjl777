f8e03641d74dec441f74dba07afc3be4cf685fc5
jl777 <jameslee777@yahoo.com>
2014-07-10 23:26:42 -0500

    b

--- NXTservices/NXTprivacy.h
+++ NXTservices/NXTprivacy.h
@@ -464,9 +464,9 @@ void after_server_read(uv_stream_t *handle,ssize_t nread,const uv_buf_t *buf)
         {
             portable_tcpwrite(handle,jsonstr,(int32_t)strlen(jsonstr)+1,-1);
             printf("free jsonstr.%p and argjson.%p\n",jsonstr,argjson);
-            //free(jsonstr);
+            //free(jsonstr); completion frees, dont do it here!
         }
-        free_json(argjson);
+        //free_json(argjson);
     }
     /*Scan for the letter Q which signals that we should quit the server. If we get QS it means close the stream.
     for (i=0; i<nread; i++)
