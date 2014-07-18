d308be941cb7a7621da748cdcbd33f97fa21121f
jl777 <jameslee777@yahoo.com>
2014-07-17 20:00:24 -0500

    -tcp

--- NXTservices/NXTprivacy.h
+++ NXTservices/NXTprivacy.h
@@ -503,12 +503,12 @@ struct NXT_acct *process_intro(uv_stream_t *handle,char *bufbase,int32_t sendres
                     init_hexbytes(pubkey,Global_mp->session_pubkey,sizeof(Global_mp->session_pubkey));
                     sprintf(argstr,"{\"NXT\":\"%s\",\"pubkey\":\"%s\",\"time\":%ld}",Server_NXTaddr,pubkey,time(NULL));
                     printf("got argstr.(%s)\n",argstr);
-                    //issue_generateToken(0,token,argstr,Server_secret);
+                    issue_generateToken(0,token,argstr,Server_secret);
                     token[NXT_TOKEN_LEN] = 0;
                     sprintf(retbuf,"[%s,{\"token\":\"%s\"}]",argstr,token);
                     if ( retbuf[0] == 0 )
                         printf("error generating intro??\n");
-                    else portable_tcpwrite(handle,retbuf,(int32_t)strlen(retbuf)+1,ALLOCWR_ALLOCFREE);
+                    //else portable_tcpwrite(handle,clonestr(retbuf),(int32_t)strlen(retbuf)+1,ALLOCWR_FREE);
                     printf("after tcpwrite to %p (%s)\n",handle,retbuf);
                 }
             } else np = 0;
