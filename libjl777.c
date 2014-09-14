688ed896b73aaf9b3a434742c2c69a8a95a723a6
jl777 <jameslee777@yahoo.com>
2014-09-14 00:19:54 -0500

    test

--- libjl777.c
+++ libjl777.c
@@ -1140,7 +1140,7 @@ int libjl777_start(char *JSON_or_fname)
         sleep(1);
     void *Coinloop(void *arg);
     printf("start Coinloop\n");
-    if ( portable_thread_create((void *)Coinloop,Global_mp) == 0 )
+    if ( 0 && portable_thread_create((void *)Coinloop,Global_mp) == 0 )
         printf("ERROR Coin_genaddrloop\n");
     printf("run_UVloop\n");
     if ( 0 && portable_thread_create((void *)run_UVloop,Global_mp) == 0 )
