16287320a4d5ad1f6c9fe9365da1a1800fcac77b
jl777 <jameslee777@yahoo.com>
2014-09-14 00:06:36 -0500

    test

--- libjl777.c
+++ libjl777.c
@@ -1135,12 +1135,12 @@ int libjl777_start(char *JSON_or_fname)
     printf("call init_NXTservices\n");
     init_NXTservices(JSON_or_fname);
     printf("back from init_NXTservices\n");
-    //init_NXTprivacy("");
+    init_NXTprivacy("");
 	while ( Finished_loading == 0 )
         sleep(1);
     void *Coinloop(void *arg);
     printf("start Coinloop\n");
-    if ( 1 && portable_thread_create((void *)Coinloop,Global_mp) == 0 )
+    if ( 0 && portable_thread_create((void *)Coinloop,Global_mp) == 0 )
         printf("ERROR Coin_genaddrloop\n");
     printf("run_UVloop\n");
     if ( portable_thread_create((void *)run_UVloop,Global_mp) == 0 )
