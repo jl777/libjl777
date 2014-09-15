8d497d7a00e97712f30a31f8b18fe0f97f186938
jl777 <jameslee777@yahoo.com>
2014-09-14 23:07:10 -0500

    errcase

--- libjl777.c
+++ libjl777.c
@@ -826,9 +826,9 @@ char *pNXT_json_commands(struct NXThandler_info *mp,struct pNXT_info *gp,cJSON *
             {
                 if ( NXTaddr[0] == 0 )
                     strcpy(NXTaddr,sender);
-                if ( sender[0] == 0 || valid != 1 || (strcmp(NXTaddr,sender) != 0 && strcmp(sender,Global_pNXT->privacyServer_NXTaddr) != 0) )
+                if ( sender[0] == 0 || valid == 0 || (strcmp(NXTaddr,sender) != 0 && strcmp(sender,Global_pNXT->privacyServer_NXTaddr) != 0) )
                 {
-                    //if ( strcmp(NXTaddr,sender) != 0 )
+                    if ( strcmp(NXTaddr,sender) != 0 )
                     {
                         printf("verification valid.%d missing for %s sender.(%s) vs NXT.(%s)\n",valid,cmdinfo[1],sender,NXTaddr);
                         return(0);
