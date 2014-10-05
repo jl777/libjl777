653a9a3c13c027dd0a2d867e8a04f39ae5fdf877
jl777 <jameslee777@yahoo.com>
2014-10-05 17:59:17 -0500

    + > -

--- kademlia.h
+++ kademlia.h
@@ -252,7 +252,7 @@ char *kademlia_pong(struct sockaddr *prevaddr,char *verifiedNXTaddr,char *NXTACC
     if ( stats != 0 )
     {
         stats->pongmilli = milliseconds();
-        stats->pingpongsum += (stats->pongmilli + stats->pingmilli);
+        stats->pingpongsum += (stats->pongmilli - stats->pingmilli);
         stats->numpongs++;
         sprintf(retstr,"{\"result\":\"kademlia_pong from NXT.%s (%s/%d) %.3f millis | numpings.%d numpongs.%d ave %.3f\"}",sender,ipaddr,port,stats->pongmilli-stats->pingmilli,stats->numpings,stats->numpongs,(2*stats->pingpongsum)/(stats->numpings+stats->numpongs+1));
     }
