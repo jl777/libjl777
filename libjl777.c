95824810367ba370f2cee9e6a4032c001810aa1f
jl777 <jameslee777@yahoo.com>
2014-10-13 14:02:14 -0500

    test

--- libjl777.c
+++ libjl777.c
@@ -1401,7 +1401,7 @@ char *SuperNET_gotpacket(char *msg,int32_t duration,char *ip_port)
     char ipaddr[64],txidstr[64],retjsonstr[2*MAX_JSON_FIELD],verifiedNXTaddr[64],*cmdstr,*retstr;
     if ( Finished_init == 0 )
         return(0);
-
+    printf("gotpacket.(%s) duration.%d from (%s)\n",msg,duration,ip_port);
     p2pport = parse_ipaddr(ipaddr,ip_port);
     uv_ip4_addr(ipaddr,p2pport,(struct sockaddr_in *)&prevaddr);
     pserver = get_pserver(0,ipaddr,0,p2pport);
