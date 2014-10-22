e67d42250941fd6bfeb934dfb80004263894d1b2
jl777 <jameslee777@yahoo.com>
2014-10-21 20:28:25 -0500

    test

--- storage.h
+++ storage.h
@@ -47,36 +47,36 @@ int32_t init_storage()
         {
             fprintf(stderr,"Error creating environment handle: %s\n",db_strerror(ret));
             return(-1);
-        }
+        } else printf("Storage environment created\n");
       	//(void)Storage->set_data_dir(Storage,"storage");
         if ( (ret= Storage->open(Storage,"storage",DB_CREATE|DB_INIT_LOCK|DB_INIT_LOG|DB_INIT_MPOOL|DB_INIT_TXN,0)) != 0 )
         {
             printf("error.%d opening Storage environment\n",ret);
             exit(ret);
-        }
+        } else printf("Storage opened\n");
     }
     if ( (ret= db_create(&Public_dbp,Storage,0)) != 0 )
     {
         printf("error.%d creating Public_dbp database\n",ret);
         return(ret);
-    }
+    } else printf("Public_dbp created\n");
     if ( (ret= db_create(&Private_dbp,Storage,0)) != 0 )
     {
         printf("error.%d creating Private_dbp database\n",ret);
         return(ret);
-    }
-  	(void)Public_dbp->set_create_dir(Public_dbp,"storage");
-	(void)Private_dbp->set_create_dir(Private_dbp,"storage");
+    } else printf("Private_dbp created\n");
+  	//(void)Public_dbp->set_create_dir(Public_dbp,"storage");
+	//(void)Private_dbp->set_create_dir(Private_dbp,"storage");
     if ( (ret= Public_dbp->open(Public_dbp,NULL,"public.db",NULL,DB_HASH,DB_CREATE | 0*DB_AUTO_COMMIT,0)) != 0 )
     {
         printf("error.%d opening Public_dbp database\n",ret);
         return(ret);
-    }
+    } else printf("Public_dbp opened\n");
     if ( (ret= Private_dbp->open(Private_dbp,NULL,"private.db",NULL,DB_HASH,DB_CREATE | 0*DB_AUTO_COMMIT,0)) != 0 )
     {
         printf("error.%d opening Private_dbp database\n",ret);
         return(ret);
-    }
+    } else printf("Private_dbp opened\n");
     return(0);
 }
 
