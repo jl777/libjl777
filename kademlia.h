d51fed71567b8fd0b1fb6e173a7353b773696579
jl777 <jameslee777@yahoo.com>
2014-11-10 19:05:30 -0500

    test

--- kademlia.h
+++ kademlia.h
@@ -208,7 +208,7 @@ int32_t sort_all_buckets(uint64_t *sortbuf,uint64_t hash)
             {
                 expand_ipbits(ipaddr,stats->ipbits);
                 pserver = get_pserver(0,ipaddr,0,0);
-                //if ( pserver->decrypterrs == 0 && (now - stats->lastcontact) < 600 )
+                if ( (now - stats->lastcontact) < 600 )//pserver->decrypterrs == 0 && 
                 {
                     sortbuf[n<<1] = bitweight(stats->nxt64bits ^ hash);// + ((stats->gotencrypted == 0) ? 64 : 0);
                     sortbuf[(n<<1) + 1] = stats->nxt64bits;
@@ -641,7 +641,7 @@ int32_t kademlia_pushstore(int32_t selector,uint64_t refbits,uint64_t newbits)
     int32_t n = 0;
     uint64_t *keys,key;
     struct storage_queue_entry *ptr;
-    //fprintf(stderr,"pushstore\n");
+    fprintf(stderr,"pushstore\n");
     if ( (keys= find_closer_Kstored(selector,refbits,newbits)) != 0 )
     {
         while ( (key= keys[n++]) != 0 )
@@ -650,7 +650,7 @@ int32_t kademlia_pushstore(int32_t selector,uint64_t refbits,uint64_t newbits)
             ptr->destbits = newbits;
             ptr->selector = selector;
             ptr->keyhash = key;
-            //printf("%p queue.%d to %llu\n",ptr,n,(long long)newbits);
+            printf("%p queue.%d to %llu\n",ptr,n,(long long)newbits);
             queue_enqueue(&storageQ,ptr);
         }
         //printf("free sps.%p\n",sps);
@@ -668,6 +668,7 @@ uint64_t process_storageQ()
     uint64_t txid = 0;
     struct SuperNET_storage *sp;
     struct coin_info *cp = get_coin_info("BTCD");
+    fprintf(stderr,"process_storageQ\n");
     if ( (ptr= queue_dequeue(&storageQ)) != 0 )
     {
         fprintf(stderr,"dequeue StorageQ %p key.(%llu) dest.(%llu) selector.%d\n",ptr,(long long)ptr->keyhash,(long long)ptr->destbits,ptr->selector);
