6c8c8bd440278e205927646871d065265d6ed8fb
jl777 <jameslee777@yahoo.com>
2014-12-17 05:26:12 -0500

    threads

--- udp.h
+++ udp.h
@@ -766,12 +766,12 @@ int32_t Do_transfers(void *_args,int32_t argsize)
     struct transfer_args *args = *(struct transfer_args **)_args;
     char datastr[4096],*retstr;
     struct coin_info *cp = get_coin_info("BTCD");
-    int32_t i,remains,num,finished,retval = -1;
+    int32_t i,remains,missing,num,finished,retval = -1;
     uint32_t now = (uint32_t)time(NULL);
     printf("Do_transfers.args.%p\n",args);
     if ( cp != 0 )
     {
-        retval = 0;
+        retval = missing = 0;
         num = finished = 0;
         remains = args->totallen;
         for (i=0; i<args->numblocks; i++)
@@ -780,7 +780,7 @@ int32_t Do_transfers(void *_args,int32_t argsize)
            //     printf("crc[%d].(%u vs %u).%d ",i,args->gotcrcs[i],args->crcs[i],args->gotcrcs[i] != args->crcs[i]);
             if ( args->gotcrcs[i] != args->crcs[i] )
             {
-                if ( num < 128 && (now - args->timestamps[i]) > 3 )
+                if ( (missing++ % 16) == 0 && (now - args->timestamps[i]) > 3 )
                 {
                     init_hexbytes_noT(datastr,args->data + i*args->blocksize,(remains < args->blocksize) ? remains : args->blocksize);
                     retstr = sendfrag(0,cp->srvNXTADDR,cp->srvNXTADDR,cp->srvNXTACCTSECRET,args->dest,args->name,i,args->numblocks,args->totallen,args->blocksize,args->totalcrc,args->crcs[i],datastr,args->handler);
