31c244c9969e45d4070494da0b042a34c576a6e4
jl777 <jameslee777@yahoo.com>
2014-12-17 01:35:50 -0500

    broadcast?

--- udp.h
+++ udp.h
@@ -254,7 +254,7 @@ uv_udp_t *open_udp(struct sockaddr *addr,void (*handler)(uv_udp_t *,ssize_t,cons
             return(0);
         }
     }
-    else
+   // else
     {
         r = uv_udp_set_broadcast(udp,1);
         if ( r != 0 )
@@ -333,7 +333,7 @@ void *start_libuv_udpserver(int32_t ip4_or_ip6,uint16_t port,void (*handler)(uv_
     struct sockaddr_in6 addr6;
     if ( ip4_or_ip6 == 4 )
     {
-        ASSERT(0 == uv_ip4_addr("0.0.0.0",0*port,&addr));
+        ASSERT(0 == uv_ip4_addr("0.0.0.0",port,&addr));
         ptr = (const struct sockaddr *)&addr;
     }
     else if ( ip4_or_ip6 == 6 )
