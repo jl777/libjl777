d6335ac965ffb73a9ad56859bbc33c80abdc468c
jl777 <jameslee777@yahoo.com>
2014-12-25 02:59:43 -0500

    fix saving file

--- mgw.h
+++ mgw.h
@@ -2668,11 +2668,8 @@ char *MGW(char *issuerNXT,int32_t rescan,int32_t actionflag,char *coin,char *ass
     else retstr = wait_for_pendingtxid(cp,specialNXTaddrs,issuerNXT,pendingtxid);
     if ( json != 0 )
     {
-        if ( NXTaddr != 0 && NXTaddr[0] != 0 )
-        {
-            retstr = cJSON_Print(json);
+        if ( NXTaddr == 0 || NXTaddr[0] == 0 )
             strcpy(NXTaddr,cp->name);
-        }
         if ( (cp= get_coin_info("BTCD")) != 0 )
             cJSON_AddItemToObject(json,"NXT",cJSON_CreateString(cp->srvNXTADDR));
         cJSON_AddItemToObject(json,"requestType",cJSON_CreateString("MGWresponse"));
