6392ddd25c52b3b4816e44b4874e58df16208c9a
jl777 <jameslee777@yahoo.com>
2015-02-23 12:16:53 +0200

    test

--- udp.h
+++ udp.h
@@ -1328,9 +1328,10 @@ char *challenge_func(char *NXTaddr,char *NXTACCTSECRET,char *previpaddr,char *se
     array = 0;
     while ( milliseconds() < endmilli )
     {
-        for (i=0; i<8; i++)
-            data[offset+i] = (rand() >> 8) & 0xff;
-        memcpy(&nonce,&data[offset],sizeof(nonce));
+        //for (i=0; i<8; i++)
+        //    data[offset+i] = (rand() >> 8) & 0xff;
+        //memcpy(&nonce,&data[offset],sizeof(nonce));
+        randombytes(&nonce,sizeof(nonce));
         hit = Hit(data,(uint32_t)(offset + sizeof(nonce)));
         printf("%llu vs %llu\n",(long long)hit,(long long)threshold);
         if ( hit < threshold )
