a3d39096627679105747b0908d394fba23b400a5
jl777 <jameslee777@yahoo.com>
2015-03-04 03:32:55 +0200

    test

--- atomic.h
+++ atomic.h
@@ -696,8 +696,8 @@ uint64_t submit_triggered_bidask(char *bidask,uint64_t nxt64bits,char *NXTACCTSE
     uint64_t txid = 0;
     char cmd[4096],*jsonstr;
     cJSON *json;
-    sprintf(cmd,"%s=%s&asset=%llu&referencedTransactionFullHash=%s&message=%s&secretPhrase=%s&feeNQT=%llu&quantityQNT=%llu&priceNQT=%llu&deadline=%d",NXTSERVER,bidask,(long long)assetid,triggerhash,comment,NXTACCTSECRET,(long long)MIN_NQTFEE,(long long)qty,(long long)NXTprice,deadline);
-    if ( (jsonstr= issue_curl(0,cmd)) != 0 )
+    sprintf(cmd,"%s=%s&asset=%llu&referencedTransactionFullHash=%s&message=%s&secretPhrase=%s&feeNQT=%llu&quantityQNT=%llu&priceNQT=%llu&deadline=%d",_NXTSERVER,bidask,(long long)assetid,triggerhash,comment,NXTACCTSECRET,(long long)MIN_NQTFEE,(long long)qty,(long long)NXTprice,deadline);
+    if ( (jsonstr= issue_NXTPOST(0,cmd)) != 0 )
     {
         stripwhite_ns(jsonstr,(int32_t)strlen(jsonstr));
         printf("submit.(%s)\n",jsonstr);
