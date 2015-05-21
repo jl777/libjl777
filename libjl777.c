9f5b1b2c32534a58ef172f9e9cf7c040e363982b
jl777 <jameslee777@yahoo.com>
2015-05-21 16:16:35 +0300

    test

--- libjl777.c
+++ libjl777.c
@@ -1330,7 +1330,7 @@ int32_t PLUGNAME(_process_json)(struct plugin_info *plugin,uint64_t tag,char *re
     FILE *fp;
     int32_t i;
     retbuf[0] = 0;
-    printf("<<<<<<<<<<<< INSIDE PLUGIN.(%s)! initflag.%d process %s\n",plugin->name,initflag,plugin->name);
+    printf("<<<<<<<<<<<< INSIDE PLUGIN.(%s)! (%s) initflag.%d process %s\n",plugin->name,jsonstr,initflag,plugin->name);
     if ( initflag > 0 )
     {
         Debuglevel = 2;
