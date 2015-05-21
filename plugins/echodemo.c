34b5f0e5a5030add2c39ee47c30ce01301d2f977
jl777 <jameslee777@yahoo.com>
2015-05-21 19:45:34 +0300

    test

--- plugins/echodemo.c
+++ plugins/echodemo.c
@@ -38,7 +38,7 @@ uint64_t PLUGNAME(_register)(struct plugin_info *plugin,STRUCTNAME *data,cJSON *
 
 int32_t PLUGNAME(_process_json)(struct plugin_info *plugin,uint64_t tag,char *retbuf,int32_t maxlen,char *jsonstr,cJSON *json,int32_t initflag)
 {
-    char echostr[MAX_JSON_FIELD],*resultstr,*str,*methodstr;
+    char echostr[MAX_JSON_FIELD],*resultstr,*methodstr;
     retbuf[0] = 0;
     printf("<<<<<<<<<<<< INSIDE PLUGIN! process %s (%s)\n",plugin->name,jsonstr);
     if ( initflag > 0 )
